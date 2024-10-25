e02cfacb236e6d60de5daec6d6f310382266697e
nikhilmujeeb <nikhilmujeeb@gmail.com>
2024-10-26 00:46:01 +0530

    first commit

--- model/user.js
+++ model/user.js
@@ -1,10 +1,15 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    name: { type: String },
 });
+
+userSchema.methods.comparePassword = function(password) {
+    return bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 export default User;
