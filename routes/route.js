20a5ccd6c343a6a32262c51befd99671c56594f1
nikhilmujeeb <nikhilmujeeb@gmail.com>
2024-10-27 01:49:46 +0530

    first commit

--- routes/route.js
+++ routes/route.js
@@ -3,11 +3,11 @@ import { createPost, updatePost, deletePost, getPost, getAllPosts } from '../con
 import { newComment, getComments, deleteComment } from '../controller/comment-controller.js';
 import { loginUser, signupUser, logoutUser } from '../controller/user-controller.js';
 import { authenticateToken, createNewToken } from '../controller/jwt-controller.js';
-import uploadRoute from './uploadRoute.js';  // Use the dedicated upload route
+import uploadRoute from './uploadRoute.js'; 
 
 const router = express.Router();
 
-router.use('/upload', uploadRoute);  // Attach upload route
+router.use('/upload', uploadRoute);
 
 router.post('/login', loginUser);
 router.post('/signup', signupUser);
@@ -19,8 +19,8 @@ router.post('/create', authenticateToken, createPost);
 router.put('/update/:id', authenticateToken, updatePost);
 router.delete('/delete/:id', authenticateToken, deletePost);
 
-router.get('/post/:id', authenticateToken, getPost);
-router.get('/posts', authenticateToken, getAllPosts);
+router.get('/post/:id', getPost);
+router.get('/posts', getAllPosts);
 
 router.post('/comment/new', authenticateToken, newComment);
 router.get('/comments/:id', authenticateToken, getComments);
