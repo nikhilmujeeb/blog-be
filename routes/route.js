22d97ad76bf0b732d45e9acab44dcb43668629c6
nikhilmujeeb <nikhilmujeeb@gmail.com>
2024-10-31 15:35:49 +0530

    first commit

--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,8 @@
 import express from 'express';
-import { createPost, updatePost, deletePost, getPost, getAllPosts } from '../controller/post-controller.js';
+import { createPost, updatePost, deletePost, getPost, getAllPosts, getAllCategories } from '../controller/post-controller.js';
 import { newComment, getComments, deleteComment } from '../controller/comment-controller.js';
 import { loginUser, signupUser, logoutUser } from '../controller/user-controller.js';
 import { authenticateToken, createNewToken } from '../controller/jwt-controller.js';
-import { getAllCategories } from '../controller/category-controller.js'; // Import the new function
 
 const router = express.Router();
 
@@ -17,7 +16,7 @@ router.put('/update/:id', authenticateToken, updatePost);
 router.delete('/delete/:id', authenticateToken, deletePost);
 router.get('/post/:id', getPost);
 router.get('/posts', getAllPosts);
-router.get('/categories', getAllCategories); // Correctly set the route for categories
+router.get('/categories', getAllCategories);
 
 router.post('/comment/new', authenticateToken, newComment);
 router.get('/comments/:id', authenticateToken, getComments);
