8f5d44b57227df046c7299f247125bd5046525d2
nikhilmujeeb <nikhilmujeeb@gmail.com>
2024-11-02 02:00:17 +0530

    first commit

--- controller/post-controller.js
+++ controller/post-controller.js
@@ -3,9 +3,8 @@ import mongoose from 'mongoose';
 
 export const createPost = async (request, response) => {
     try {
-        const post = new Post(request.body); 
-        await post.save(); 
-
+        const post = new Post(request.body);
+        await post.save();
         response.status(200).json('Post saved successfully');
     } catch (error) {
         console.error('Error creating post:', error.message);
@@ -25,11 +24,11 @@ export const updatePost = async (request, response) => {
         if (!post) return response.status(404).json({ msg: 'Post not found' });
 
         const updatedPost = {
-            ...post.toObject(), 
-            ...request.body, 
+            ...post.toObject(),
+            ...request.body,
         };
 
-        await Post.findByIdAndUpdate(id, { $set: updatedPost });
+        await Post.findByIdAndUpdate(id, { $set: updatedPost }, { new: true });
         response.status(200).json('Post updated successfully');
     } catch (error) {
         console.error('Error updating post:', error.message);
@@ -80,13 +79,14 @@ export const getAllPosts = async (request, response) => {
     let category = request.query.category;
     let posts;
     try {
-        if (username) 
+        if (username) {
             posts = await Post.find({ username: username });
-        else if (category) 
+        } else if (category) {
             posts = await Post.find({ categories: category });
-        else 
+        } else {
             posts = await Post.find({});
-        
+        }
+
         response.status(200).json(posts);
     } catch (error) {
         console.error('Error retrieving posts:', error.message);
